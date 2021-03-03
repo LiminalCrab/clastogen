9ccc7a50d0042636bc09d628e908f8e084ad4eaa
LiminalCrab <devpreston@protonmail.com>
2021-03-02 20:40:56 -0500

    tokens 1.0.2 - meaningful symbols can be pushed to array [no regex yet]

--- clastogen.js
+++ clastogen.js
@@ -1,11 +1,19 @@
 // Need to figure out what I will use to identify new tokens.
 // Lexer?
 
+
 function genToken(inStream){
-        let vrtStream = inStream.trim().split('');
+        let vrtStream = inStream.split('');
         let symbols = [];
         console.log("vrtStream:", vrtStream);
-        symbols.push(vrtStream);
-        return symbols;
-}
 
+        for(index = 0; index < vrtStream.length; index++){
+            console.log(vrtStream[index]);
+            if (vrtStream[index].match("T")){
+                console.log(`Assigning: ${vrtStream[index].length} meaningful symbols to array.`);
+                symbols.push(vrtStream[index]);
+            } else {
+                console.log("Failure.")
+        }
+    }
+}
