2378ded7308cbfe186bb935bfe1ca15e516ad521
LiminalCrab <devpreston@protonmail.com>
2021-03-03 04:40:22 -0500

    trying nested functions

--- clastogen.js
+++ clastogen.js
@@ -1,26 +1,22 @@
 //Stage 1
+function genToken(inStream){
+    let regr = RegExp(/{\$([^\$]+)\$}/, 'g')
+    let array;
+    let symbols = [];
+    let vrtStream = inStream.trim();
+    console.log("Extracting meaningful symbols from string: ", vrtStream);
 
-function clastogen(data){ 
-    function genToken(inStream){
-            let regr = RegExp(/{\$([^\$]+)\$}/, 'g')
-            let array;
-            let symbols = [];
-            let vrtStream = inStream.trim();
-            console.log("Extracting meaningful symbols from string: ", vrtStream);
+    while((array = regr.exec(vrtStream)) !== null){
+        symbols.push(array)
+        console.log(`Meaningful symbol: ${array[0]} found. Next starts at ${regr.lastIndex}`)
+    }
 
-            while((array = regr.exec(vrtStream)) !== null){
-                symbols.push(array)
-                console.log(`Meaningful symbol: ${array[0]} found. Next starts at ${regr.lastIndex}`)
-            }
-
-            for(index = 0; index < symbols.length; index++){
-                if(symbols[index]){
-                    console.log(`Assigned: ${symbols[index].length} meaningful symbols to array`)
-                } else {
-                    console.log("There are no meaningful symbols to assign.")
-                }
-                return console.log(symbols);
-            }
-    } // end of genToken
-
-} // end of clastoge n
+    for(index = 0; index < symbols.length; index++){
+        if(symbols[index]){
+            console.log(`Assigned: ${symbols[index].length} meaningful symbols to array`)
+        } else {
+            console.log("There are no meaningful symbols to assign.")
+        }
+        return symbols;
+    }
+} // end of genToken
